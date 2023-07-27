53a7d99703d2ca46757cb501e51dd05c59ea4fb0
Marco <100785809+Marco5dev@users.noreply.github.com>
2023-07-27 21:56:54 +0300

    some better things

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const d2023Router = require("./routers/d2023Router");
 
 // mongoose
 mongoose
-  .connect(OD, { useNewUrlParser: true })
+  .connect(LD, { useNewUrlParser: true })
   .then(() =>
     console.log("\x1b[32m[successfully]:\x1b[0m Connected to local MongoDB")
   )
@@ -63,6 +63,35 @@ app.use("/d2020", d2020Router);
 app.use("/d2021", d2021Router);
 app.use("/d2022", d2022Router);
 app.use("/d2023", d2023Router);
+
+app.get("/test", async (req, res) => {
+  try {
+    // Perform multiple Mongoose operations
+    const d2018Data = await D2018.find();
+    const d2019Data = await D2019.find();
+    const d2020Data = await D2020.find();
+    const d2021Data = await D2021.find();
+    const d2022Data = await D2022.find();
+    const d2023Data = await D2023.find();
+    const TopData = await Top.find();
+
+    res.render("dashboard.ejs", {
+      arr2018: d2018Data,
+      arr2019: d2019Data,
+      arr2020: d2020Data,
+      arr2021: d2021Data,
+      arr2022: d2022Data,
+      arr2023: d2023Data,
+      arrTop: TopData,
+
+      title: "St.George",
+      description: "صفحه كنيسه الشهيد العظيم مارجرجس ببورسعيد",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // server
 app.listen(port, () => {
