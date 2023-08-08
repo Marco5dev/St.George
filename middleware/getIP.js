af4e292d597d7fc039923e5a04ec9e532f8cce2a
Marco <100785809+Marco5dev@users.noreply.github.com>
2023-08-09 02:26:53 +0300

    #

--- middleware/getIP.js
+++ middleware/getIP.js
@@ -1,22 +1,24 @@
+// middleware/checkIPAddress.js
 const axios = require('axios');
 
 const checkIPAddress = async (req, res, next) => {
   const allowedIP = '197.54.107.214';
-  
+
   try {
-    const response = await axios.get('http://api.ipify.org?format=json');
-    const publicIP = response.data.ip;
+    const response = await axios.get('http://httpbin.org/ip');
+    const responseData = response.data;
+    const publicIPv4 = responseData.origin;
 
-    console.log('Server Public IP:', publicIP);
+    console.log('Server Public IPv4:', publicIPv4);
 
-    // Compare the public IP with the allowed IP
-    if (publicIP === allowedIP) {
+    // Compare the public IPv4 with the allowed IP
+    if (publicIPv4 === allowedIP) {
       next();
     } else {
       res.status(404).send('Not Found');
     }
   } catch (error) {
-    console.error('Error fetching public IP:', error.message);
+    console.error('Error fetching public IPv4:', error.message);
     res.status(500).send('Internal Server Error');
   }
 };
