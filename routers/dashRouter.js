176964793cad8f589569eff8b4a2e49892157a27
Marco <100785809+Marco5dev@users.noreply.github.com>
2023-08-23 09:09:05 +0300

    #

--- routers/dashRouter.js
+++ routers/dashRouter.js
@@ -6,6 +6,7 @@ const upload = require("../middleware/upload");
 const uuid = require("uuid");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const fs = require("fs").promises;
 
 const dataFolderPath = path.join(__dirname, "../Data");
 const jsonDB = new JSONDatabase(dataFolderPath);
@@ -41,25 +42,36 @@ router.use(async (req, res, next) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const enteredUsername = req.body.username;
   const enteredPassword = req.body.password;
   const rememberMe = req.body.remember === "on";
 
-  // Perform authentication (replace with your authentication logic)
-  const validUsername = process.env.USER1;
-  const validPassword = process.env.PASS1;
+  const usersFilePath = path.join(dataFolderPath, "data.dAdmin.json"); // Correct path to users.json
 
-  if (enteredUsername === validUsername && enteredPassword === validPassword) {
-    if (rememberMe) {
-      res.cookie("dashboard_login", "true"); // Set the cookie
+  try {
+    const usersData = await fs.readFile(usersFilePath, "utf8");
+    const users = JSON.parse(usersData);
+
+    const validUser = users.find(
+      (user) =>
+        user.username === enteredUsername && user.password === enteredPassword
+    );
+
+    if (validUser) {
+      if (rememberMe) {
+        res.cookie("dashboard_login", "true"); // Set the cookie
+      }
+      res.redirect("/dash");
+    } else {
+      res.render("loginForm.ejs", {
+        title: "Login",
+        description: "Incorrect credentials. Please try again.",
+      });
     }
-    res.redirect("/dash");
-  } else {
-    res.render("loginForm.ejs", {
-      title: "Login",
-      description: "Incorrect credentials. Please try again.",
-    });
+  } catch (error) {
+    console.error("Error reading users file:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
@@ -88,45 +100,65 @@ router.get("/", (req, res) => {
 
 // Add data
 router.get("/add", (req, res) => {
-  try {
-    res.render("add.ejs", {
-      title: "Add Data",
-      description: "Add new data to the website",
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).render("500-2.ejs", {
-      title: "500 Internal server error",
-      description: "Sorry, something went wrong. Please try again later.",
+  const isLoggedIn = req.cookies["dashboard_login"] === "true";
+  if (isLoggedIn) {
+    console.log("User is authenticated");
+    try {
+      res.render("add.ejs", {
+        title: "Add Data",
+        description: "Add new data to the website",
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).render("500-2.ejs", {
+        title: "500 Internal server error",
+        description: "Sorry, something went wrong. Please try again later.",
+      });
+    }
+  } else {
+    // User is not authenticated
+    res.render("loginForm.ejs", {
+      title: "Login",
+      description: "Please enter your credentials to access the dashboard.",
     });
   }
 });
 
 router.post("/add", upload.single("image"), async (req, res) => {
-  try {
-    const { dataName, name, social, rank, competition, date, edu } = req.body;
+  const isLoggedIn = req.cookies["dashboard_login"] === "true";
+  if (isLoggedIn) {
+    console.log("User is authenticated");
+    try {
+      const { dataName, name, social, rank, competition, date, edu } = req.body;
 
-    const uniqueID = uuid.v4();
-    const newData = {
-      id: uniqueID,
-      social,
-      image: req.file.dataName,
-      name,
-      rank,
-      competition,
-      date,
-      edu,
-    };
+      const uniqueID = uuid.v4();
+      const newData = {
+        id: uniqueID,
+        social,
+        image: req.file.dataName,
+        name,
+        rank,
+        competition,
+        date,
+        edu,
+      };
 
-    await jsonDB.addData(dataName, newData);
+      await jsonDB.addData(dataName, newData);
 
-    res.redirect(`/dash`);
-    console.log(req.file);
-  } catch (err) {
-    console.error(err);
-    res.status(500).render("500-2.ejs", {
-      title: "500 Internal server error",
-      description: "Sorry, something went wrong. Please try again later.",
+      res.redirect(`/dash`);
+      console.log(req.file);
+    } catch (err) {
+      console.error(err);
+      res.status(500).render("500-2.ejs", {
+        title: "500 Internal server error",
+        description: "Sorry, something went wrong. Please try again later.",
+      });
+    }
+  } else {
+    // User is not authenticated
+    res.render("loginForm.ejs", {
+      title: "Login",
+      description: "Please enter your credentials to access the dashboard.",
     });
   }
 });
@@ -135,26 +167,36 @@ router.post("/add", upload.single("image"), async (req, res) => {
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
 
-  try {
-    const result = await jsonDB.findDataById(id);
+  const isLoggedIn = req.cookies["dashboard_login"] === "true";
+  if (isLoggedIn) {
+    console.log("User is authenticated");
+    try {
+      const result = await jsonDB.findDataById(id);
 
-    if (result) {
-      res.render("data-1", {
-        title: result.name,
-        description: result.competition,
-        item: result,
-      });
-    } else {
-      res.status(404).render("404.ejs", {
-        title: "Not Found",
-        description: "Sorry, the requested data was not found.",
+      if (result) {
+        res.render("data-1", {
+          title: result.name,
+          description: result.competition,
+          item: result,
+        });
+      } else {
+        res.status(404).render("404.ejs", {
+          title: "Not Found",
+          description: "Sorry, the requested data was not found.",
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).render("500-2.ejs", {
+        title: "500 Internal server error",
+        description: "Sorry, something went wrong. Please try again later.",
       });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).render("500-2.ejs", {
-      title: "500 Internal server error",
-      description: "Sorry, something went wrong. Please try again later.",
+  } else {
+    // User is not authenticated
+    res.render("loginForm.ejs", {
+      title: "Login",
+      description: "Please enter your credentials to access the dashboard.",
     });
   }
 });
@@ -162,12 +204,23 @@ router.get("/:id", async (req, res) => {
 // Delete data by ID
 router.delete("/:id", async (req, res) => {
   const id = req.params.id;
-  try {
-    await jsonDB.deleteDataById(id);
-    res.sendStatus(200);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("An error occurred while deleting data");
+
+  const isLoggedIn = req.cookies["dashboard_login"] === "true";
+  if (isLoggedIn) {
+    console.log("User is authenticated");
+    try {
+      await jsonDB.deleteDataById(id);
+      res.sendStatus(200);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("An error occurred while deleting data");
+    }
+  } else {
+    // User is not authenticated
+    res.render("loginForm.ejs", {
+      title: "Login",
+      description: "Please enter your credentials to access the dashboard.",
+    });
   }
 });
 
@@ -175,26 +228,36 @@ router.delete("/:id", async (req, res) => {
 router.get("/edit/:id", async (req, res) => {
   const id = req.params.id;
 
-  try {
-    const result = await jsonDB.findDataById(id);
+  const isLoggedIn = req.cookies["dashboard_login"] === "true";
+  if (isLoggedIn) {
+    console.log("User is authenticated");
+    try {
+      const result = await jsonDB.findDataById(id);
 
-    if (result) {
-      res.render("edit.ejs", {
-        title: "Edit Data",
-        description: "Edit the selected data item",
-        item: result,
-      });
-    } else {
-      res.status(404).render("404.ejs", {
-        title: "Not Found",
-        description: "Sorry, the requested data was not found.",
+      if (result) {
+        res.render("edit.ejs", {
+          title: "Edit Data",
+          description: "Edit the selected data item",
+          item: result,
+        });
+      } else {
+        res.status(404).render("404.ejs", {
+          title: "Not Found",
+          description: "Sorry, the requested data was not found.",
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).render("500-2.ejs", {
+        title: "500 Internal server error",
+        description: "Sorry, something went wrong. Please try again later.",
       });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).render("500-2.ejs", {
-      title: "500 Internal server error",
-      description: "Sorry, something went wrong. Please try again later.",
+  } else {
+    // User is not authenticated
+    res.render("loginForm.ejs", {
+      title: "Login",
+      description: "Please enter your credentials to access the dashboard.",
     });
   }
 });
@@ -204,26 +267,37 @@ router.post("/edit/:id", upload.single("image"), async (req, res) => {
   const id = req.params.id;
   const { name, social, rank, competition, date, edu } = req.body;
 
-  const newData = {
-    social,
-    image: req.file.filename,
-    name,
-    rank,
-    competition,
-    date,
-    edu,
-  };
+  const isLoggedIn = req.cookies["dashboard_login"] === "true";
+  if (isLoggedIn) {
+    console.log("User is authenticated");
 
-  const updatedData = newData;
+    const newData = {
+      social,
+      image: req.file.filename,
+      name,
+      rank,
+      competition,
+      date,
+      edu,
+    };
 
-  try {
-    await jsonDB.editData(id, updatedData);
-    res.redirect(`/dash/${id}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).render("500-2.ejs", {
-      title: "500 Internal server error",
-      description: "Sorry, something went wrong. Please try again later.",
+    const updatedData = newData;
+
+    try {
+      await jsonDB.editData(id, updatedData);
+      res.redirect(`/dash/${id}`);
+    } catch (err) {
+      console.error(err);
+      res.status(500).render("500-2.ejs", {
+        title: "500 Internal server error",
+        description: "Sorry, something went wrong. Please try again later.",
+      });
+    }
+  } else {
+    // User is not authenticated
+    res.render("loginForm.ejs", {
+      title: "Login",
+      description: "Please enter your credentials to access the dashboard.",
     });
   }
 });
